184b7a8dc3c86cf37d416e59270b613f2b963e58
Niquo <niquo@mit.edu>
2015-12-16 16:11:09 +0000

    added code to also use the bag of words approach for each page rather than a bag of links

--- classify.py
+++ classify.py
@@ -14,6 +14,10 @@ Creating a classifer based on the links from the leaf pages.
 
 categoriesLinks: List of Strings. This is a list of the links that represent the categories we want to use as 
 					as our classification. Again, this is only the suffix of the full URL
+
+useLinks: Boolean indicating whether we will be creating our probabilites on either the bag of all words on the page
+			or the links that occur on the page.
+
 max_depth: Integer. How far do we want our crawling to actually traverse from these root nodes? Specify here, but 
 			be aware that the number of pages has the potential to grow exponentially with the depth.
 
@@ -28,7 +32,7 @@ totals: list of integers of length M that represets the total number of occurenc
 keyDict: a dictionary where the key is a string of the form "wiki/example_page" and the value is a number i 0<= i <= m i.e the value 
 		is the index in the matrix and total vector that represents the number of occurences of the key (link). 
 """
-def createClassifier(categoriesLinks,max_depth=1):
+def createClassifier(categoriesLinks,useLinks=True,max_depth=1):
 	allCategories = [] #will be used to store the categories types of the initial links
 	allCatsLinks = [] #basically a copy of the categoriesLinks, but we move over tho this list if any were invalid
 	base = 'https://en.wikipedia.org'
@@ -42,26 +46,46 @@ def createClassifier(categoriesLinks,max_depth=1):
 	occurMatrix = []
 	totals = []
 	max_cat = 0
-	for cat in allCategories:
-		cat.collectLeafPages()
-		catOccurences = [0]*max_cat
-		# print 'length of subpages is ', len(cat.subPages)
-		for pg in cat.subPages:
-			for link in pg.links:
-				if link not in keyDict:
-					keyDict[link] = max_cat
-					catOccurences.append(1)
-					totals.append(1)
-					max_cat += 1
-				else:
-					index = keyDict[link]
-					catOccurences[index] += 1
-					totals[index] += 1
-		occurMatrix.append(catOccurences)
-		# print 'the current maximum number of categories is',max_cat,'for category',cat.link
+	if useLinks:
+		for cat in allCategories:
+			cat.collectLeafPages()
+			catOccurences = [0]*max_cat
+			# print 'length of subpages is ', len(cat.subPages)
+			for pg in cat.subPages:
+				for link in pg.links:
+					if link not in keyDict:
+						keyDict[link] = max_cat
+						catOccurences.append(1)
+						totals.append(1)
+						max_cat += 1
+					else:
+						index = keyDict[link]
+						catOccurences[index] += 1
+						totals[index] += 1
+			occurMatrix.append(catOccurences)
+			# print 'the current maximum number of categories is',max_cat,'for category',cat.link
 
-	for row in occurMatrix:
-		row += [0]*(max_cat - len(row))
+		for row in occurMatrix:
+			row += [0]*(max_cat - len(row))
+	else:
+		for cat in allCategories:
+				cat.collectLeafPages()
+				catOccurences = [0]*max_cat
+				for pg in cat.subPages:
+					for word,n in pg.words.iteritems():
+						if word not in keyDict:
+							keyDict[word] = max_cat
+							catOccurences.append(n)
+							totals.append(n)
+							max_cat += 1
+						else:
+							index = keyDict[word]
+							catOccurences[index] += n
+							totals[index] += n
+				occurMatrix.append(catOccurences)
+
+			for row in occurMatrix:
+				row += [0]*(max_cat - len(row))
 
 	return allCatsLinks,occurMatrix,totals,keyDict
 
@@ -92,7 +116,7 @@ distributions: a list of tuples where the first value in the tuple is the catego
 				probability that the given page belongs to that category. 
 
 """
-def naiveBayes(classPage,allCatsLinks,occurMatrix,totals,keyDict,epsilon=1e-2):
+def naiveBayes(classPage,allCatsLinks,occurMatrix,totals,keyDict,epsilon=1e-1):
 	distribution = [1]*len(allCatsLinks)
 	for link in classPage.links:
 		for i in xrange(len(allCatsLinks)):
@@ -108,7 +132,6 @@ def naiveBayes(classPage,allCatsLinks,occurMatrix,totals,keyDict,epsilon=1e-2):
 			else:
 				distribution[i] *= epsilon
 	new_dist = normalize(distribution)
-
 	return [(allCatsLinks[i],new_dist[i]) for i in range(len(allCatsLinks))]
 
 
