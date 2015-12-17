57c355d73ea7dc647647c810cbb5aaea79be272d
Niquo <niquo@mit.edu>
2015-12-17 15:20:43 +0000

    added comments and moved the categories to be an input the my quick generate method rather than hard code the exampe categories

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import page
 import classify
 import pickle
 
+"""
+This method generates prior probabilities for a list of category link suffixes.
 
-def generatePriorOnGivenWikiCategories(depth=1,links=True):
-	rareDisLink = '/wiki/Category:Rare_diseases'
-	infecDisLink = '/wiki/Category:Infectious_diseases'
-	cancerLink = '/wiki/Category:Cancer'
-	congenitialDis = '/wiki/Category:Congenital_disorders'
-	organsLink = '/wiki/Category:Organs_(anatomy)'
-	MLLinks = '/wiki/Category:Machine_learning_algorithms'
-	medDevLinks = '/wiki/Category:Medical_devices'
-	catLinks = [rareDisLink,infecDisLink,cancerLink,congenitialDis,organsLink,MLLinks,medDevLinks]
-	# catLinks = [cancerLink,organsLink] #a smaller subset of the links if I want to test the algorithm for higher depths
+***INPUT***
+catsLinks: list of strings where each string is a valid categories suffix link
+depth: positive integer to represent how far we want to recurse from the root categories. (recommend 1 to save computation time)
+links: Boolean indicating whether the classifier will use the links or bag of words approach. True implies use links False -> B.O.W.
+
+***OUTPUT***
+creates serialized files containing the output objects of the createClassifier method.
+Boolean indicating that function was successful.
+
+"""
+def generatePriorOnGivenWikiCategories(catsLinks,depth=1,links=False):
 	print 'getting prior probabilities for the given categories...'
 	allCatsLinks,occurMatrix,totals,keyDict,useLinks = classify.createClassifier(catLinks,links,depth)
 
@@ -25,8 +28,18 @@ def generatePriorOnGivenWikiCategories(depth=1,links=True):
 
 	print "prior probabilites have been serialized for depth =",depth
 
-	return None	
+	return True	
 
+"""
+checkPage takes in a wikipedia page suffix and finds the distribution over the serialized categories and data.
+
+***INPUT***
+link: String representing the suffix of a wikipedia page that will soon be classified among categories that are established already.
+
+***OUTPUT***
+Prints the name each category and how likely the page is to belong to that category.
+True if completely successfully
+"""
 def checkPage(link):
 	checkPage = page.Page(link)
 
@@ -40,9 +53,17 @@ def checkPage(link):
 	for result in distribution:
 		print link,'is a subpage of', result[0], 'with probability', round(result[1]*100,5),'%'
 
-	return None
+	return True
 
+"""
+uses pickle to serialize and object to a file.
+***INPUT***
+obj: Object to be serialized
+filename: string representing the filename where the object will be stored.
 
+***OUTPUT**
+True if successfully else False
+"""
 def serialize(obj,filename):
 	try:
 		pickle.dump(obj,open(filename,'wb'))
@@ -51,6 +72,14 @@ def serialize(obj,filename):
 		return False
 	return True
 
+"""
+retreives a serialized object
+***INPUT***
+filename: string representing the filename where the object is stored
+
+***OUTPUT***
+the object that was stored in the file or false if there was an error
+"""
 def unpack(filename):
 	try:
 		return pickle.load(open(filename,"rb"))
@@ -58,6 +87,9 @@ def unpack(filename):
 		print "Unexpected error:", sys.exc_info()[0]
 		return False
 
+"""
+Follow the rabbit. 
+"""
 def main():
 	already_made = False
 	while True:
@@ -98,9 +130,14 @@ def main():
 		allCategoriesLinks = []
 		print 'Please give the input in the form \"/wiki/Category:Example_category\".'
 		print 'If a category is not valid input or not recognized, it will be dropped by the classifier.'
-		for i in xrange(int(catCount)):
+		inc = 0
+		while inc < count:
 			catLink = raw_input("enter your category: ")
-			allCategoriesLinks.append(catLink)
+			if categories.isCategory(catLink):
+				allCategoriesLinks.append(catLink)
+				inc += 1
+			else:
+				print "invalid category format"
 
 		print 'Creating prior probilities for naive bayes clssification...'
 		allCatsLinks,occurMatrix,totals,keyDict,useLinks = classify.createClassifier(allCategoriesLinks,useLinks,depth)
@@ -136,7 +173,18 @@ def main():
 
 if __name__ == "__main__":
 	main()
-	# generatePriorOnGivenWikiCategories(1,False)
+
+	rareDisLink = '/wiki/Category:Rare_diseases'
+	infecDisLink = '/wiki/Category:Infectious_diseases'
+	cancerLink = '/wiki/Category:Cancer'
+	congenitialDis = '/wiki/Category:Congenital_disorders'
+	organsLink = '/wiki/Category:Organs_(anatomy)'
+	MLLinks = '/wiki/Category:Machine_learning_algorithms'
+	medDevLinks = '/wiki/Category:Medical_devices'
+	catLinks = [rareDisLink,infecDisLink,cancerLink,congenitialDis,organsLink,MLLinks,medDevLinks]
+
+	# generatePriorOnGivenWikiCategories(catLinks,1,False)
+
 	KernelMethod = '/wiki/Kernel_methods_for_vector_output' #pulled from machine learning topics
 	tobacco = '/wiki/Tobacco' #pulled from cancer
 	neonatal = '/wiki/Neonatal_sepsis' #pulled from infectious diseases
@@ -144,5 +192,5 @@ if __name__ == "__main__":
 	kuru = '/wiki/Kuru_(disease)' #rare diseases
 	DIX = '/wiki/DIXDC1' #cancer
 	nausea = '/wiki/Cancer_and_nausea'
-	# checkPage(tobacco)
+	# checkPage(KernelMethod)
 
